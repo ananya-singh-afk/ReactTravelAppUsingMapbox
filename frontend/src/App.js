a2b2214f6a6fe69186ddcf8857909ab9f989d749
Ananya Singh <singh.ananya2580@gmail.com>
2024-10-15 21:17:01 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Map from "react-map-gl";
 import { Marker } from "react-map-gl";
-import RoomIcon from '@mui/icons-material/Room';
-import { Icon } from '@mui/material';
-
+import "mapbox-gl/dist/mapbox-gl.css";
+import RoomIcon from "@mui/icons-material/Room";
 
 function App() {
+  const [viewport] = React.useState({
+    zoom: 9
+  });
   return (
     <div className="App">
       <Map
@@ -13,12 +15,23 @@ function App() {
         initialViewState={{
           longitude: 78.4,
           latitude: 20.8,
-          zoom: 4,
+          zoom: 4
         }}
-        style={{ width: "40vw", height: "40vh" }}
+        style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        <Marker latitude={18.7577} longitude={72.4376}>
+        <Marker
+          latitude={21.6}
+          longitude={74}
+          anchor="bottom"
+        >
+          <RoomIcon
+            style={{
+              fontSize: viewport.zoom * 5,
+              color: "slateblue",
+              cursor: "pointer",
+            }}
+          ></RoomIcon>
         </Marker>
       </Map>
     </div>
