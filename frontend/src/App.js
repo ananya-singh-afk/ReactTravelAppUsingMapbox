55f52c9e2f39f5d6500faf190ec0b6c305272e07
Ananya Singh <singh.ananya2580@gmail.com>
2024-10-22 21:57:22 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,12 @@ function App() {
   const currentUser = "Jane"
   const [pins, setPins] = useState([]);
   const [currenPlaceId, setCurrenPlaceId] = useState(null);
-  const [viewport] = React.useState({
-    zoom: 9,
+  const [newPlace, setNewPlace] = useState(null);
+  const [viewport, setViewport] = useState({
+    width: "100vw", height: "100vh",
+    latitude: 20.8,
+    longitude: 78.4,
+    zoom: 4,
   });
   useEffect(() => {
     const getPins = async () => {
@@ -28,21 +32,32 @@ function App() {
     getPins()
   }, []);
 
-  const handleMarkerClick = (id) => {
+  const handleMarkerClick = (id,latitude,longitude) => {
     setCurrenPlaceId(id);
+    setViewport({...viewport, latitude:latitude, longitude:longitude});
   }
+
+  const handleAddClick = (e)=>{
+    const [lat, long] = [e.lngLat.lat, e.lngLat.lng];
+    setNewPlace({
+      lat,
+      long
+    });
+  };
 
   return (
     <div className="App">
       <Map
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
         initialViewState={{
-          longitude: 78.4,
-          latitude: 20.8,
-          zoom: 4,
+          longitude: viewport.longitude,
+          latitude: viewport.latitude,
+          zoom: viewport.zoom,
         }}
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
+        onDblClick={handleAddClick}
+        transitionDuration="200"
       >
         {pins.map(p => (
           <>
@@ -53,15 +68,15 @@ function App() {
             >
               <RoomIcon
                 style={{
-                  fontSize: viewport.zoom * 5,
+                  fontSize: 30,
                   color: p.username===currentUser? "tomato" :"slateblue",
                   cursor: "pointer",
                 }}
-                onClick = {() => handleMarkerClick(p._id)}
+                onClick = {() => handleMarkerClick(p._id, p.latitude, p.longitude)}
               ></RoomIcon>
             </Marker>
             {(p._id === currenPlaceId) && 
-            <Popup longitude={p.longitude} latitude={p.latitude} anchor="left"
+            <Popup longitude={p.longitude} latitude={p.latitude} anchor="left" closeOnClick={false}
             onClose={() => setCurrenPlaceId(null)}>
               <div className="card">
                 <label>Place</label>
@@ -86,6 +101,28 @@ function App() {
             }
           </>
           ))}
+          {newPlace && (
+         <Popup longitude={newPlace.long} latitude={newPlace.lat} anchor="left" closeOnClick={false}
+            onClose={() => setNewPlace(null)}> 
+            <div>
+              <form>
+                <label> Title </label>
+                <input placeholder="Enter a title"/>
+                <label> Review </label>
+                <textarea placeholder="Enter your review"/>
+                <label> Rating </label>
+                <select>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className="submitButton" type="submit"> Add Pin </button>
+              </form>
+            </div>
+         </Popup>
+         )}
       </Map>
     </div>
   );
